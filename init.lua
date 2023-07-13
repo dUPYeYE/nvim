d3d9a6b9b11cc103cceccc28c18bfb4f74a5ae5f
dUPYeYE <robert.boros12@gmail.com>
2023-07-13 21:56:33 +0300

    Changed minimap colors

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ require("dupp.packer")
 vim.cmd([[
   augroup CustomColors
     autocmd!
-    autocmd ColorScheme * highlight minimapCursor   ctermbg=59  ctermfg=228 guibg='#5F5F5F' guifg='#FFFF87'
-    autocmd ColorScheme * highlight minimapRange    ctermbg=242 ctermfg=228 guibg='#4F4F4F' guifg='#FFFF87'
+    autocmd ColorScheme * highlight minimapCursor   ctermbg=59  ctermfg=228 guibg='#9399b2' guifg='#94e2d5'
+    autocmd ColorScheme * highlight minimapRange    ctermbg=242 ctermfg=228 guibg='#585b70' guifg='#a6e3a1'
   augroup END
 ]])
